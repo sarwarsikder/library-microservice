19edad1ef31f181ca40f7446fb144935e300ef20
sarwarsikder <russelaiub08@gmail.com>
2020-09-11 16:30:56 +0600

    rest framework added
    Show book list in the as Rest API

--- library_project/settings.py
+++ library_project/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    #3rd Party Apps/Plugins
+    'rest_framework',
+    #Apps
     'books.apps.BooksConfig'
 ]
 
